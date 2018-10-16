b360b1e556b5045910f504e001f2e89fcecc5f92
Benjamin Hall <b.e.s.hall@outlook.com>
2018-10-16 17:58:46 +0100

    Initial Commit

---
+++ TextStyles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextStyles
+//
+//  Created by beshbashbosh on 16/10/2018.
+//  Copyright © 2018 beshbashbosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
